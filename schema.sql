2ffc5cd9428625b688ce23ef33ef0c5306a813fc
Emerald May <32318838+Duchess-May@users.noreply.github.com>
2023-10-26 02:10:24 -0700

    First file upload

---
+++ schema.sql
@@ -0,0 +1,11 @@
+/my_blog/
+|-- /templates/
+|   |-- index.html
+|   |-- login.html
+|   |-- register.html
+|   |-- dashboard.html
+|-- /static/
+|   |-- styles.css
+|   |-- placeholder.jpg
+|-- app.py
+|-- schema.sql
